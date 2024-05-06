ada5a8a24d2a5e72803f2c240422edaf56fd93dd
Snail26 <141910498+Snail26@users.noreply.github.com>
2024-05-06 18:02:07 +1200

    Starting json version

---
+++ nodejs.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+//fs preinstalled in node, no need for package.json
+const dbName = "database.json";
+
+function addPath(path) {
+    var jsonDB = JSON.parse(fs.readFileSync(dbName));
+    var lastPathes = [];
+    path.replace("]", "").split("[").forEach((path) => {
+        if (lastPathes[0] = undefined) {
+            lastPathes.unshift(path);
+        }
+        if (eval(`jsonDB[${lastPathes.join("][")}]`)) {
+
+        }
+    });
+}
+setPathValue("[`hai`][`hello`]", "[1, 2, 3]")
+function setPathValue(itemPath, value) {
+    var jsonDB = JSON.parse(fs.readFileSync(dbName).toString());
+    eval(`jsonDB${itemPath} = {"value": ${value}};`);
+    fs.writeFileSync(dbName, JSON.stringify(jsonDB));
+}
+
+function getItem() {
+    return(JSON.parse(fs.readFileSync(dbName)));
+}
--- setup.bat
+++
@@ -1,2 +0,0 @@
-(
-    echo main ^{^})>"database.snailbase"
