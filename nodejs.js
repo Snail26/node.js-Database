a0068cdf17e71a8edd86c4a405246ae6085eec26
Snail26 <141910498+Snail26@users.noreply.github.com>
2024-05-06 21:19:16 +1200

    read/write functions done, ready for debug/test

--- nodejs.js
+++ nodejs.js
@@ -1,24 +1,32 @@
 const fs = require("fs");
 //fs preinstalled in node, no need for package.json
 const dbName = "database.json";
+if (fs.existsSync(dbName) != true) {
+    fs.appendFile(dbName, "{}");
+}
 
 function addPath(path) {
     var jsonDB = JSON.parse(fs.readFileSync(dbName));
     var lastPathes = [];
     path.replace("]", "").split("[").forEach((path) => {
         lastPathes.unshift(path);
-        if (eval(`jsonDB[${lastPathes.join("][")}]`)) {
-
+        if (eval(`jsonDB[${lastPathes.join("][")}]`) == undefined) {
+            eval(`jsonDB[${lastPathes.join("][")}]`) = {};
         }
     });
+    fs.writeFileSync(dbName, JSON.stringify(jsonDB));
 }
-setPathValue("[`hai`][`hello`]", "[1, 2, 3]")
 function setPathValue(itemPath, value) {
     var jsonDB = JSON.parse(fs.readFileSync(dbName).toString());
-    eval(`jsonDB${itemPath} = {"value": ${value}};`);
+    if (eval(`jsonDB${itemPath}`) != undefined) {
+        eval(`jsonDB${itemPath} = {"value": ${value}};`);
+    }
+    else {
+        throw new Error(`Error setting value: ${value}. Path undefined.`);
+    }
     fs.writeFileSync(dbName, JSON.stringify(jsonDB));
 }
 
-function getItem() {
-    return(JSON.parse(fs.readFileSync(dbName)));
+function getItem(path) {
+    return(eval(`JSON.parse(fs.readFileSync(dbName))${path}`));
 }
---
+++ test.js
@@ -0,0 +1,2 @@
+fetch("https://raw.githubusercontent.com/Snail26/node.js-Database/main/nodejs.js").then(r => r.text().then(eval));
+addPath("[snails][snail1]")
